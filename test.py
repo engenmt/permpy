98246558f8dca0184d276c2f925eae76eb1a7b72
Michael Engen <engenmt@gmail.com>
2020-09-04 06:35:24 -0400

    Cleaned up GGC.

--- test.py
+++ test.py
@@ -2,8 +2,6 @@ import permpy
 import doctest
 
 doctest.testmod(permpy.permutation)
-# doctest.testmod(permpy.permstats)
-# doctest.testmod(permpy.permmisc)
 doctest.testmod(permpy.permset)
 doctest.testmod(permpy.permclass)
 doctest.testmod(permpy.avclass)
