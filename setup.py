9ddbf2dfc8f2c72967fa771d2717d1fd6b07481f
Michael Engen <engenmt@gmail.com>
2022-07-08 19:53:23 -0400

    Moved package contents deeper, added various packaging tools.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 try:
     from setuptools import setup
@@ -6,13 +6,20 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name="permpy",
-    version="0.0.4",
-    description="Permutation (patterns) workshop in Python",
+    name="permpy-engenmt",
+    version="0.0.12",
+    description="A package for dealing with permutation patterns.",
     author="Michael Engen and Cheyne Homberger and Jay Pantone",
     author_email="cheyne.homberger@gmail.com",
+    maintainer="Michael Engen",
+    maintainer_email="engenmt@gmail.com",
     url="https://github.com/cheyneh/permpy",
     keywords=["permutations", "patterns", "research", "enumeration"],
     classifiers=[],
-    packages=["permpy", "permpy.InsertionEncoding", "permpy.RestrictedContainer"],
+    packages=[
+        "permpy",
+        "permpy.InsertionEncoding",
+        "permpy.RestrictedContainer",
+        "permpy.deprecated",
+    ],
 )
